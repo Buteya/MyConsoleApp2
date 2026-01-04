8671631381bcc03e9ef689ba5e1b4c588643e0b5
Buteya <buteyamasira@gmail.com>
2026-01-04 23:53:31 +0300

    file showing c# learning path

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current date and time is " + DateTime.Now);
+Console.WriteLine("Hello, World! I'm Steve Masira. Nice to meet you here.");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first line of code!");
+Console.WriteLine();
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first line of code!");
+Console.WriteLine();
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
