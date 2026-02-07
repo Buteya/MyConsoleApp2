20376363ea59b74b6e8ebc1e4035da3dbd4e542d
buteya <buteya@BUTEYA>
2026-02-07 12:26:50 +0300

    added new code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+using System;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("The current date and time is " + DateTime.Now);
 Console.WriteLine("Hello, World! I'm Steve Masira. Nice to meet you here.");
@@ -621,13 +624,145 @@ while(true)
         Console.WriteLine($"The role name that you entered, \"{inputToCheck}\" is not valid. Enter your role name (administrator, manager, user)");
     }
 }
-String[] myString = new String[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-int[] periodLocation = new int[] { };
+String[] myStrings = new String[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
 
-for(int i = 0; i < myString.Length; i++)
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
 {
-    for(int j = 0;j < myString[i].Length; j++)
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    // extract sentences from each string and display them one at a time
+    while (periodLocation != -1)
     {
-        Console.WriteLine(myString[i]);
+
+        // first sentence is the string value to the left of the period location
+        mySentence = myString.Remove(periodLocation);
+
+        // the remainder of myString is the string value to the right of the location
+        myString = myString.Substring(periodLocation + 1);
+
+        // remove any leading white-space from myString
+        myString = myString.TrimStart();
+
+        // update the comma location and increment the counter
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
     }
+
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
 }
+Console.WriteLine("===================================================");
+Console.WriteLine("new code");
+Console.WriteLine("===================================================");
+
+// the ourAnimals array will store the following: 
+string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+
+// variables that support data entry
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
+
+// array used to store runtime data, there is no persisted data
+string[,] ourAnimals = new string[maxPets, 6];
+
+// TODO: Convert the if-elseif-else construct to a switch statement
+
+// create some initial ourAnimals array entries
+for (int i = 0; i < maxPets; i++)
+{
+    if (i == 0)
+    {
+        animalSpecies = "dog";
+        animalID = "d1";
+        animalAge = "2";
+        animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+        animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+        animalNickname = "lola";
+    }
+    else if (i == 1)
+    {
+        animalSpecies = "dog";
+        animalID = "d2";
+        animalAge = "9";
+        animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+        animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+        animalNickname = "loki";
+    }
+    else if (i == 2)
+    {
+        animalSpecies = "cat";
+        animalID = "c3";
+        animalAge = "1";
+        animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+        animalPersonalityDescription = "friendly";
+        animalNickname = "Puss";
+    }
+    else if (i == 3)
+    {
+        animalSpecies = "cat";
+        animalID = "c4";
+        animalAge = "?";
+        animalPhysicalDescription = "";
+        animalPersonalityDescription = "";
+        animalNickname = "";
+    }
+    else
+    {
+        animalSpecies = "";
+        animalID = "";
+        animalAge = "";
+        animalPhysicalDescription = "";
+        animalPersonalityDescription = "";
+        animalNickname = "";
+    }
+
+    ourAnimals[i, 0] = "ID #: " + animalID;
+    ourAnimals[i, 1] = "Species: " + animalSpecies;
+    ourAnimals[i, 2] = "Age: " + animalAge;
+    ourAnimals[i, 3] = "Nickname: " + animalNickname;
+    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+}
+
+// display the top-level menu options
+
+Console.Clear();
+
+Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+Console.WriteLine(" 1. List all of our current pet information");
+Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+Console.WriteLine(" 5. Edit an animal’s age");
+Console.WriteLine(" 6. Edit an animal’s personality description");
+Console.WriteLine(" 7. Display all cats with a specified characteristic");
+Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+Console.WriteLine();
+Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+readResult = Console.ReadLine();
+if (readResult != null)
+{
+    menuSelection = readResult.ToLower();
+}
+
+Console.WriteLine($"You selected menu option {menuSelection}.");
+Console.WriteLine("Press the Enter key to continue");
+
+// pause code execution
+readResult = Console.ReadLine();
+
