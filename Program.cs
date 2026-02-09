d508dfd402368fc9cadefa5d081a41b8b0ab5036
buteya <buteya@BUTEYA>
2026-02-09 17:52:16 +0300

    added new code

--- Program.cs
+++ Program.cs
@@ -684,50 +684,55 @@ string[,] ourAnimals = new string[maxPets, 6];
 // create some initial ourAnimals array entries
 for (int i = 0; i < maxPets; i++)
 {
-    if (i == 0)
-    {
+    switch (i) { 
+    case 0:
+    
         animalSpecies = "dog";
         animalID = "d1";
         animalAge = "2";
         animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
         animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
         animalNickname = "lola";
-    }
-    else if (i == 1)
-    {
+        break;
+    case  1:
+    
         animalSpecies = "dog";
         animalID = "d2";
         animalAge = "9";
         animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
         animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
         animalNickname = "loki";
-    }
-    else if (i == 2)
-    {
+        break;
+    
+    case 2:
+    
         animalSpecies = "cat";
         animalID = "c3";
         animalAge = "1";
         animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
         animalPersonalityDescription = "friendly";
         animalNickname = "Puss";
-    }
-    else if (i == 3)
-    {
+        break;
+    
+    case  3:
+    
         animalSpecies = "cat";
         animalID = "c4";
         animalAge = "?";
         animalPhysicalDescription = "";
         animalPersonalityDescription = "";
         animalNickname = "";
-    }
-    else
-    {
+        break;
+    
+     default: 
+    
         animalSpecies = "";
         animalID = "";
         animalAge = "";
         animalPhysicalDescription = "";
         animalPersonalityDescription = "";
         animalNickname = "";
+        break;
     }
 
     ourAnimals[i, 0] = "ID #: " + animalID;
@@ -739,6 +744,8 @@ for (int i = 0; i < maxPets; i++)
 }
 
 // display the top-level menu options
+do {
+
 
 Console.Clear();
 
@@ -760,9 +767,57 @@ if (readResult != null)
     menuSelection = readResult.ToLower();
 }
 
-Console.WriteLine($"You selected menu option {menuSelection}.");
-Console.WriteLine("Press the Enter key to continue");
+//Console.WriteLine($"You selected menu option {menuSelection}.");
+//Console.WriteLine("Press the Enter key to continue");
 
 // pause code execution
-readResult = Console.ReadLine();
-
+//readResult = Console.ReadLine();
+    switch (menuSelection)
+    {
+        case "1":
+            // List all of our current pet information
+            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "2":
+            // Add a new animal friend to the ourAnimals array
+            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "3":
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "4":
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "5":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "6":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "7":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        case "8":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+        default:
+            break;
+    }
+   
+} while (menuSelection != "exit");
