e886da7ebadc74d2c35a0ae4beebffb99656522e
buteya <buteya@BUTEYA>
2026-02-25 16:44:16 +0300

    added new code

--- Program.cs
+++ Program.cs
@@ -992,3 +992,24 @@ Console.WriteLine("Floating point types:");
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 Console.WriteLine($"decimal: {decimal.MinValue}  to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+int val_A = 2;
+int val_B = val_A;
+val_B = 5;
+
+Console.WriteLine("--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
+
+
+int[] ref_A = new int[3];
+ref_A[0] = 2;
+int[] ref_B = ref_A;
+ref_B[0] = 5;
+Console.WriteLine("--Refrence Types--");
+Console.WriteLine($"ref_A: {ref_A[0]}");
+Console.WriteLine($"ref_B: {ref_B[0]}");
+
+
+
+
