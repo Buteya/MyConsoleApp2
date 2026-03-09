c97e69e8ddd9e8b04178704d55367ca1def45527
buteya <buteya@BUTEYA>
2026-03-09 12:29:32 +0300

    added new code

--- Program.cs
+++ Program.cs
@@ -1085,3 +1085,28 @@ foreach(var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"clearing 2 ...count {pallets.Length}");
+foreach(var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"resizing to 6...count {pallets.Length}");
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach(var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"resting to 3...count {pallets.Length}");
+foreach(var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+
