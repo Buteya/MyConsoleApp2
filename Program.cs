e6cfe6a7b7f5a22a11c950fa88d6b10d78b81ef0
buteya <buteya@BUTEYA>
2026-05-12 21:26:32 +0300

    added new code

--- Program.cs
+++ Program.cs
@@ -1458,6 +1458,59 @@ void AdjustTimes()
         times[i] = ((times[i] + diff)) % 2400;
     }
 }
+Console.WriteLine("===================================");
+Console.WriteLine("New code");
+Console.WriteLine("===================================");
+string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
+string[] address;
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
 
+foreach(string ip in ipv4Input)
+{
+    address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+    validateLength();
+    validateZeroes();
+    validateRange();
 
+    if(validLength && validZeroes && validRange)
+    {
+        Console.WriteLine($"{ip}, is a valid IPv4 address.");
+    } else
+    {
+        Console.WriteLine($"{ip}, is an invalid IPv4 address.");
+    }
+} 
 
+void validateLength()
+{
+    validLength = address.Length == 4;
+}
+
+void validateZeroes()
+{
+    foreach(string number in address)
+    {
+        if(number.Length > 1 && number.StartsWith("0"))
+        {
+            validZeroes = false;
+            return;
+        }
+    }
+    validZeroes = true;
+}
+
+void validateRange()
+{
+    foreach(string number in address)
+    {
+        int value = int.Parse(number);
+        if(value < 0 || value > 255)
+        {
+            validRange = false;
+            return;
+        }
+    }
+    validRange = true;
+}
